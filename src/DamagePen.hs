7ee044c345188a1e764fd621031217ab41264166
jack <steve.a.shogren@gmail.com>
2017-02-22 07:25:14 -0500

    Getting the dps calc to compile

--- src/DamagePen.hs
+++ src/DamagePen.hs
@@ -2,6 +2,8 @@ module DamagePen where
 
 import Optimization
 import Data.List
+import Data.Ord
+import Data.Function (on)
 
 attackSpeed :: Fractional a => a -> a -> a
 attackSpeed bat asm =(1/bat)*asm
@@ -15,19 +17,12 @@ noBonusCrit = 1.5
 armorPointsToNum :: Num a => a -> a
 armorPointsToNum ar = ar * 7
 
--- old formula?
--- dmgReduction :: Fractional a => a -> a -> a -> a
--- dmgReduction ar pn lvl =
---   let armor = ar + 31.5
---       pen = pn * 4.0
---   in (armor-pen) / (100+(armor-pen)+(10*(lvl-1)))
-
 dmgReduction2 :: Double -> Double -> Double
 dmgReduction2 armorpts penpts =
   let effectiveArmor = 31.5 + (7 * armorpts) - (penpts * 4.0)
       realArmor = if (effectiveArmor < 0) then 0 else effectiveArmor
       reduction = (100/(100 + effectiveArmor))
-  in if reduction > 1 then 1 else (fromInteger reduction)
+  in if reduction > 1 then 1 else reduction
 
 dps :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double
 dps power_points attack_speed_points crit_points pen crit_damage base_damage base_attack_speed scaling lvl =
@@ -37,8 +32,8 @@ dps power_points attack_speed_points crit_points pen crit_damage base_damage bas
       crit_bonus = (1+((0.04*crit_points)*(crit_damage-1)))
   in base_dmg * hits_second * crit_bonus * reduction
 
-murdockDps :: Integer -> Integer -> Integer -> Integer -> Integer -> Double
-murdockDps pwr speed crit pen bonus = dps pwr speed crit pen bonus 86 1.16 1 15
+murdockDps :: Integer -> Integer -> Integer -> Integer -> Double -> Double
+murdockDps pwr speed crit pen bonus = dps (fromInteger pwr) (fromInteger speed) (fromInteger crit) (fromInteger pen) bonus 86 1.16 1 15
 
 toBuild (dmg,speed,crit,pen,critbonus) =
   Build { _bpower = dmg
@@ -59,17 +54,18 @@ calcIfUnder dmg speed crit pen critbonus max =
   else (0, toBuild (0,0,0,0,0))
 
 maxDps =
-  let totalPoints = 61
+  let totalPoints = 66
       lifeSteal = 6
       ward = 1
-      points = totalPoints - lifeSteal - ward
+      blink = 1
+      points = totalPoints - lifeSteal - ward - (5 * blink)
       totals = [ (calcIfUnder dmg speed crit pen critbonus points) |
                  dmg <- [0..30],
                  speed <- [0..30],
                  crit <- [0..30],
                  pen <- [0..30],
                  critbonus <- [0..1]]
-      (dps,build) = head $ reverse $ sortBy (\(a,_) (b,_) -> sort a b) totals
+      (dps,build) = head $ sortBy (flip compare `on` fst) totals
   in
     (show dps)
 
