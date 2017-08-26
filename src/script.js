794f84314653a94fd330f6732f0be8a2a2bbae05
jack <steve.a.shogren@gmail.com>
2017-08-26 08:27:41 -0400

    Updating scraper for new cards

--- src/script.js
+++ src/script.js
@@ -1,13 +1,17 @@
 var cards = $(".card");
 var realCards = [];
+var cardStrs = [];
 cards.each(function(i, elm) {
     var card = $(this);
     var name = card.find(".t-n").text();
-    var cost = card.find(".t-p").text();
-    var afinity = card.find(".t-c").text();
+    var cost = card.attr("price");
+    var afinity = card.attr("school");
     var does = card.find("tr:nth-child(3)").text();
     var does2 = card.find("tr:nth-child(4)").text();
     realCards.push({name: name, cost: cost, afinity: afinity, does: does});
-    $("#contents").append(name + ', ' +  cost + ', ' +  afinity + ', ' +   does + '|' +  does2 + '<br>');
+    //$("#contents").append(name + ', ' +  cost + ', ' +  afinity + ', ' +   does + '|' +  does2 + '<br>');
+    cardStrs.push(name + ', ' +  cost + ', ' +  afinity + ', ' +   does + '|' +  does2 + '<br>');
 });
 
+//console.log(realCards[0]);
+console.log(cardStrs[0]);
